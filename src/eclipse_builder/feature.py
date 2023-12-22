dd934885c9bf507ab55071b22ffb1d543868e4d8
Laurent Almeras <lalmeras@gmail.com>
2023-12-22 16:03:07 +0100

    try to remove uninstall/install behavior
    This was a workaround. Not needed for 2023-12, we try to remove it.

--- src/eclipse_builder/feature.py
+++ src/eclipse_builder/feature.py
@@ -26,13 +26,6 @@ LOGBACK_CONFIGURATION = """<configuration>
 """
 
 
-PROTECTED_FEATURES = set([
-    'epp.package.java',
-    'epp.package.rcp',
-    'epp.package.jee'
-])
-
-
 def install_features(eclipse_home, features, repositories, java_home=None,
                      proxy_host=None, proxy_port=3128):
     """Install features in an Eclipse instance"""
@@ -66,38 +59,15 @@ def _install_features(logback_file, eclipse_home, features, repositories,
         vmargs.insert(0, '-vmargs')
     eclipse_bin = os.path.join(os.path.abspath(eclipse_home), 'eclipse')
 
-    list_args = [
-        eclipse_bin,
-        '-noSplash', '-application', 'org.eclipse.equinox.p2.director',
-        '-listInstalledRoots'
-    ]
-    list_args.extend(vm)
-    list_args.extend(vmargs)
-    installed_features = [
-        line.split('/')[0]
-        for line in subprocess.check_output(list_args)
-                              .decode(sys.stdout.encoding)
-                              .splitlines()
-        if line
-        and 'org.eclipse.m2e.logback.configuration' not in line
-        and 'Operation completed' not in line
-        and '/' in line
-    ]
-    print(installed_features)
-
     to_install_features = set()
     to_install_features = set([feature for feature in features])
-    to_uninstall_features = set(installed_features) - PROTECTED_FEATURES
-    to_install_features.update(to_uninstall_features)
     print("installing %s" % (' '.join(to_install_features),))
-    print("uninstalling %s" % (' '.join(to_uninstall_features),))
 
     args = [
         eclipse_bin,
         '-nosplash', '-application', 'org.eclipse.equinox.p2.director',
         '-repository', ','.join(repositories),
-        '-installIU', ','.join(to_install_features),
-        '-uninstallIU', ','.join(to_uninstall_features)
+        '-installIU', ','.join(to_install_features)
     ]
     args.extend(vm)
     args.extend(vmargs)
